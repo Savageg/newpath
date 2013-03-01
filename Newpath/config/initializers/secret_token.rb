2828bd5966b4daa43842ce6d80a515d079902850
Grant Savage <sav4g33@gmail.com>
2013-03-01 11:19:38 -0500

    first commit

---
+++ Newpath/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newpath::Application.config.secret_token = '2b16091359d5d8ea41a0988060e7cda954a84357af8d4ed776e0b38b156d0adc1ab4417998458852cf6cb2edeb6ca908b4a00c4ca12b166f0f928df1772dbf6d'
